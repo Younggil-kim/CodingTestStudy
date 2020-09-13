5ba785b944296d98079e221dfecee6d96cd0d23a
김영길 <gom991@ajou.ac.kr>
2020-09-13 18:19:03 +0900

    20/09/10

--- 브루트 포스/BaekJoon7568.py
+++ 브루트 포스/BaekJoon7568.py
@@ -1,22 +1,41 @@
 #백준 7568
 
+# 2020-09-13 다시 풀었을 때
+#문제 해석
+#몸무게,키 의 형태로 주어지는데
+#둘다 커야 덩치가 크다
+# 집단이 주어질 때 덩치의 순위를 표시할 것
+
 #생각
-# 덩치가 둘다 커야 큰것.
-# 그냥 조건문으로 전체 경우 다 돌리면 가능할듯
+# 경우를 다 돌리면서 둘 다 큰 경우에만
+# +1을 해 줄것
 
 N = int(input())
-
 lst = list()
 for i in range(N):
     lst.append(input().split())
 
-re_lst = list()
+
 for i in range(N):
     grade = 1
     for j in range(N):
         if lst[i][0] < lst[j][0] and lst[i][1] < lst[j][1]:
             grade = grade + 1
-    re_lst.append(grade)
+    print(grade,end=" ")
 
-for i in re_lst:
-    print(i, end=' ')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
